8b88541eb9d1db0bd8ea4f1a07994c22909b8c31
Alex Filatau <filatau@gmail.com>
2016-01-13 12:05:29 -0800

    Add list of diners with correspondent functionality - counters of visits, button to indicate visits, support for labels, select and deselct diner

--- source/js/app.js
+++ source/js/app.js
@@ -14,26 +14,31 @@ function initMap() {
 
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
+  // remove map-loader placeholder
+  viewModel.isMapLoaded(true);
   // render seed markers only when map available
   viewModel.renderMarkers();
-};
+}
 
 /* seed data array */
 var seedDiners = [
   {
       name: "Fast Lane Coffee",
+      labels: ["asian", "pizza", "coffee"],
       lat: 37.376773,
       lng: -122.030157,
       id: "4d9a662a674ca14376eaba43"
   },
   {
       name: "Second Lane Coffee",
+      labels: ["asian", "pizza", "coffee"],
       lat: 37.38,
       lng: -122.04,
       id: "4d9a662a674ca14376eaba43"
   },
   {
       name: "Third Lane Coffee",
+      labels: ["asian", "pizza", "coffee"],
       lat: 37.3699,
       lng: -122.0299,
       id: "4d9a662a674ca14376eaba43"
@@ -46,6 +51,9 @@ var Diner = function (rawData) {
   self.name = ko.observable(rawData.name);
   self.coordinates = {lat: rawData.lat, lng: rawData.lng};
   self.id = ko.observable(rawData.id);
+  self.labels = ko.observableArray(rawData.labels);
+
+  self.visitCounter = ko.observable('');
 
   self.setMarker = function () {
     var marker = new google.maps.Marker({
@@ -55,14 +63,15 @@ var Diner = function (rawData) {
     });
 
     marker.setMap(map);
-  }
+  };
 
-}
+};
 
 var DinersViewModel = function (rawDinersData) {
   var self = this;
 
   self.selectedDiner = ko.observable();
+  self.isMapLoaded = ko.observable(false);
 
   self.listOfDiners = ko.observableArray([]);
   rawDinersData.forEach(function (diner) {
@@ -70,15 +79,30 @@ var DinersViewModel = function (rawDinersData) {
   });
 
   self.selectDiner = function(diner) {
+    self.selectNoDiner();
     self.selectedDiner(diner);
+    // TODO show info box on map
+  };
+
+  self.selectNoDiner = function() {
+    self.selectedDiner(null);
+    // TODO remove info box from map
+  };
+
+  self.visitDiner = function(diner) {
+    var counter = diner.visitCounter();
+    if (counter === '') {
+      counter = 0;
+    }
+    diner.visitCounter(counter + 1);
   };
 
   self.renderMarkers = function() {
     self.listOfDiners().forEach(function (diner) {
       diner.setMarker();
     });
-  }
-}
+  };
+};
 
 var viewModel = new DinersViewModel(seedDiners);
 ko.applyBindings(viewModel);
